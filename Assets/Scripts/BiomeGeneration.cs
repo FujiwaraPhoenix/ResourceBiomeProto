63ed56bc30dfc75cb4af33741504f849e780451e
Nick <neissanicholas@gmail.com>
2018-10-07 15:00:12 -0400

    WorldGeneration Script Implemented
    I've created the prefabs for the tiles, made them individual biomes with specific asset integration gameobjects that we can pick and drop in the inspector.  Draft 01

--- Assets/Scripts/BiomeGeneration.cs
+++ Assets/Scripts/BiomeGeneration.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class BiomeGeneration : MonoBehaviour {
     //these gameobjects will be the specific prefabs for the objects that will exist within the world(e.g trees, boulders, ponds, etc.)
-    public GameObject terrainAsset1;
-    public GameObject terrainAsset2;
-    public GameObject terrainAsset3;
+    public GameObject desertTerrainAsset1;
+    public GameObject desertTerrainAsset2;
+    public GameObject desertTerrainAsset3;
+
+    public GameObject grassTerrainAsset1;
+    public GameObject grassTerrainAsset2;
+    public GameObject grassTerrainAsset3;
+
+    public GameObject iceTerrainAsset1;
+    public GameObject iceTerrainAsset2;
+    public GameObject iceTerrainAsset3;
 
     //these floats will determine how many of each asset will populate the world upon the creation of the individual biome
     public float amountOfAsset1;
@@ -14,57 +22,119 @@ public class BiomeGeneration : MonoBehaviour {
     public float amountOfAsset3;
 
     Vector3 spawnPosition;
-
-    public Material grassTexture;
-    public Material desertTexture;
-    public Material iceTexture;
+    bool isDesert = false;
+    bool isGrass = false;
+    bool isIce = false;
 
     // Use this for initialization
     void Start () {
         //this will ensure that the biome has a randomly picked color upon activation
-        this.GetComponent<SpriteRenderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
+        if (this.gameObject.tag == "DesertTile")
+        {
+            isDesert = true;
+        }
+        if (this.gameObject.tag == "GrassTile")
+        {
+            isGrass = true;
+        }
+        if (this.gameObject.tag == "IceTile")
+        {
+            isIce = true;
+        }
+        this.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
         float textureNumber = Random.Range(0, 3);
-        if(textureNumber > 0 && textureNumber < 1f)
-        {
-            GetComponent<SpriteRenderer>().material = grassTexture;
-        }
-        if (textureNumber > 1f && textureNumber < 2f)
-        {
-
-        }
-        if (textureNumber > 2f && textureNumber < 3f)
-        {
-
-        }
 
         //these for statements will populate the world accordingly with how mnay of a specific asset you want at randomized locations with 
         //randomized size as well.
-        for (int i = 0; i < amountOfAsset1; i++)
+        if (isDesert)
         {
-            spawnPosition = new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), -1);
-            GameObject biome = (GameObject)Instantiate(terrainAsset1, spawnPosition, Quaternion.identity);
-            biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
-            biome.transform.SetParent(this.transform);
-            biome.transform.localPosition = spawnPosition;
-            
-        }
-        for (int i = 0; i < amountOfAsset2; i++)
-        {
-            spawnPosition = new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), -1);
-            GameObject biome = (GameObject)Instantiate(terrainAsset2, spawnPosition, Quaternion.identity);
-            biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
-            biome.transform.SetParent(this.transform);
-            biome.transform.localPosition = spawnPosition;
+            for (int i = 0; i < amountOfAsset1; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(desertTerrainAsset1, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
 
-        }
-        for (int i = 0; i < amountOfAsset3; i++)
-        {
-            spawnPosition = new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), -1);
-            GameObject biome = (GameObject)Instantiate(terrainAsset3, spawnPosition, Quaternion.identity);
-            biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
-            biome.transform.SetParent(this.transform);
-            biome.transform.localPosition = spawnPosition;
+            }
+            for (int i = 0; i < amountOfAsset2; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(desertTerrainAsset2, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
 
+            }
+            for (int i = 0; i < amountOfAsset3; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(desertTerrainAsset3, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
+
+            }
+        }
+        if (isGrass)
+        {
+            for (int i = 0; i < amountOfAsset1; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(grassTerrainAsset1, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
+
+            }
+            for (int i = 0; i < amountOfAsset2; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(grassTerrainAsset2, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
+
+            }
+            for (int i = 0; i < amountOfAsset3; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(grassTerrainAsset3, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
+
+            }
+        }
+        if (isIce)
+        {
+            for (int i = 0; i < amountOfAsset1; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(iceTerrainAsset1, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
+
+            }
+            for (int i = 0; i < amountOfAsset2; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(iceTerrainAsset2, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
+
+            }
+            for (int i = 0; i < amountOfAsset3; i++)
+            {
+                spawnPosition = new Vector3(Random.Range(-0.65f, 0.65f), -1f, Random.Range(-0.65f, 0.65f));
+                GameObject biome = (GameObject)Instantiate(iceTerrainAsset3, transform.position, Quaternion.identity);
+                biome.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), 1);
+                biome.transform.SetParent(this.transform);
+                biome.transform.localPosition = spawnPosition;
+
+            }
         }
     }
 
